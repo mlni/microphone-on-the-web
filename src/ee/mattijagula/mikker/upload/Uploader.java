12ace0c4790e86b45a28c13c7eec021fd80c51c8
Matti Jagula <matti.jagula@gmail.com>
2010-11-30 22:46:43 +0200

    cleaned up old uploader code

--- src/ee/mattijagula/mikker/upload/Uploader.java
+++ src/ee/mattijagula/mikker/upload/Uploader.java
@@ -10,11 +10,8 @@ import org.apache.commons.httpclient.methods.multipart.Part;
 import org.apache.commons.httpclient.methods.multipart.PartSource;
 
 import java.io.ByteArrayInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class Uploader {
     private static final String BOUNDARY = "----WebKitFormBoundary8NHXoPOgtdmTKB7e";
@@ -37,7 +34,6 @@ public class Uploader {
             throws IOException, UploadFailedException {
         HttpClient client = new HttpClient();
 
-        // PostMethod post = new PostMethod("http://localhost/~matti/recorder/upload.php");
         PostMethod post = new PostMethod(ctx.getUploadUrl());
 
         post.addRequestHeader("User-Agent", ctx.getUserAgent());
@@ -77,57 +73,5 @@ public class Uploader {
         } else {
             throw new UploadFailedException("Upload failed with code " + HttpStatus.getStatusText(status));
         }
-    }
-
-    public void upload_old(byte content[]) throws IOException {
-        URL url = new URL(ctx.getUploadUrl());
-        HttpURLConnection theUrlConnection = (HttpURLConnection) url.openConnection();
-        theUrlConnection.setRequestMethod("POST");
-        theUrlConnection.setDoOutput(true);
-        theUrlConnection.setDoInput(true);
-        theUrlConnection.setUseCaches(false);
-
-        theUrlConnection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
-        if (!"".equals(ctx.getCookies()))
-            theUrlConnection.setRequestProperty("Cookie", ctx.getCookies());
-        theUrlConnection.setRequestProperty("User-Agent", ctx.getUserAgent());
-
-        DataOutputStream httpOut = new DataOutputStream(theUrlConnection.getOutputStream());
-
-        String str = "--" + BOUNDARY + "\r\n"
-                   + "Content-Disposition: form-data; name=\"" + ctx.getFileFieldName() + "\"; filename=\"" + ctx.getUploadFilename() + "\"\r\n"
-                   + "Content-Type: " + ctx.getUploadMimeType() + "\r\n"
-                   + "\r\n";
-
-        httpOut.write(str.getBytes());
-
-        httpOut.write(content);
-
-        KeyValuePairParser.Pair postParameters[] = ctx.getAdditionalPostParameters();
-        for (KeyValuePairParser.Pair pair : postParameters) {
-            String name = pair.key;
-            String value = pair.value;
-            httpOut.write(("\r\n--" + BOUNDARY + "\r\n").getBytes());
-            httpOut.write(("Content-Disposition: form-data; name=\""+ name +"\"\r\n" +
-                    "\r\n").getBytes());
-            httpOut.write(value.getBytes());
-        }
-
-        // last one gets extra hypens at the end
-        httpOut.write(("\r\n--" + BOUNDARY + "--\r\n").getBytes());
- 
-        httpOut.flush();
-        httpOut.close();
-
-        // read & parse the response
-        InputStream is = theUrlConnection.getInputStream();
-        StringBuilder response = new StringBuilder();
-        byte[] respBuffer = new byte[4096];
-        int len;
-        while ((len = is.read(respBuffer)) >= 0) {
-            response.append(new String(respBuffer, 0, len).trim());
-        }
-        is.close();
-        System.out.println(response.toString());
     }
 }
