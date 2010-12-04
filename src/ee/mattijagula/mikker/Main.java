52020157db37d9a58f29ef2c368ec9729da5916a
Matti Jagula <matti.jagula@gmail.com>
2010-12-04 15:22:37 +0200

    left sources out of applet jar

--- src/ee/mattijagula/mikker/Main.java
+++ src/ee/mattijagula/mikker/Main.java
@@ -10,25 +10,25 @@ public class Main {
         Configuration.ParameterSource src = new Configuration.ParameterSource() {
             public String getParameter(String name) {
                 if ("upload_url".equals(name))
-                    return "http://localhost/~matti/recorder/upload.php";
+                    return "/~matti/recorder/upload.php";
                 if ("post_parameters".equals(name))
                     return "MAX_FILE_SIZE=20000000; tx_fileupload_pi1[do_upload]=Saada fail!";
                 if ("upload_field_name".equals(name))
-                    return "tx_fileupload_pi1";
+                    return "fail";
                 return null;
             }
             public URI getBaseUri() {
                 try {
-                    return new URI("http://localhost/~matti/recorder/upload.php");
+                    return new URI("http://localhost/");
                 } catch (URISyntaxException e) {
                     throw new RuntimeException(e);
                 }
             }
             public String getCookies() {
-                return "chuck=Norris";
+                return "tracking=cookie";
             }
             public String determineUserAgent() {
-                return "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.44 Safari/534.7";
+                return "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.215 Safari/534.10";
             }
         };
 
